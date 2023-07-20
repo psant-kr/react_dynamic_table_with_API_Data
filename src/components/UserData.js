05571180592bc8bf3d7a145860f8daf6e053b988
prashant kumar <prashantdoa1996@gmail.com>
2023-07-20 17:45:37 +0530

    table logic and full css added

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const UserData = ({users}) => {
+
+const UserData = ({ users }) => {
   return (
     <>
-    
+      {
+        users.map((user) => {
+          const { id, name, email } = user;
+          const { city, street, zipcode } = user.address;
+          return (
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{email}</td>
+              <td>{`City : ${city}, Street : ${street}, ZipCode : ${zipcode}`}</td>
+            </tr>
+
+          )
+
+        })
+      }
     </>
   )
 }
